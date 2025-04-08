344d16d271d43f7c10495770c75bbaa3ebf170c8
default <Adrina.peighambarzadeh@stu.ocsb.ca>
2025-04-08 16:27:58 +0000

    init

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="utf-8" />
-  <meta name="description" content="Volume of sphere, PHP" />
+  <meta name="description" content="Convert Fahrenheit to Celsius, PHP" />
   <meta name="keywords" content="math, volume, PHP, sphere" />
   <meta name="author" content="Adrina Peighambarzadeh" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -14,7 +14,7 @@
   <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
   <link rel="manifest" href="site.webmanifest" />
-  <title>Volume of sphere, PHP</title>
+  <title>Convert Fahrenheit to Celsius, PHP</title>
 </head>
 
 <body>
@@ -22,21 +22,23 @@
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">Volume of sphere, PHP</span>
+        <span class="mdl-layout-title">Convert Fahrenheit to Celsius, PHP</span>
       </div>
     </header>
     <main class="mdl-layout__content">
       <div class="right-image">
-        <img src="./images/Screenshot 2025-04-02 12.44.35.png" alt="sphere" width=250px>
+        <img src="./images/image (1).png" alt="temptaure pic" width=250px>
       </div>
       <br>
       <div class="page-content">
-        <p>Please enter intgers for radius:</p>
+        <div class="page-content-heading">formula:(32°F − 32) ×5/9 = 0°C</div>
+        <p>Please enter indgree in Fahrenheit:</p>
       </div>
+      <label class="page-content">Dgree in Fahrenheit: </label>
       <form action="answer.php" method="GET">
         <div class="mdl-textfield mdl-js-textfield">
-          <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" name="radius-of-sphere">
-          <label class="mdl-textfield__label" for="hours-worked">radius of sphere (mm)</label>
+          <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" name="dgree-in-fahrenheit">
+          <label class="mdl-textfield__label" for="hours-worked">dgree in Fahrenheit(°F)</label>
           <span class="mdl-textfield__error">Input is not a number!</span>
         </div>
         <br>
